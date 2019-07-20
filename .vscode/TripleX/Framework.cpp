f2ea497f356c28fee1a92efef56eed4b095c99cf
Keebler750 <keebler750@gmail.com>
2019-07-20 15:47:38 -0600

    Initial upload to cloud, Git Hub, from SourceTree client on local.

---
+++ .vscode/TripleX/Framework.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+// Declarations
+
+
+// Functions
+void Introduction()     // Title, info, etc
+{
+
+}
+
+void MenuSystem()       // Play, exit, set level, etc
+{
+
+}
+
+void DoLevel()         // Guts of level generation here
+{
+
+}
+
+void GenerateNumbers()  // Generate numbers into possible reference or pointer vars.
+{
+
+}
+
+void PlayGame()         // GameState control, PlayerInput, etc.
+{
+
+}
+
+
+
+// Entry
+int main()
+{
+
+
+    return 0;
+}
